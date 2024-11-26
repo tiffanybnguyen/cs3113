e820461ca5d54c37019546990c4da22e8e83b7b8
Tiffany Nguyen <tiffanynguyenb4@gmail.com>
2024-11-26 16:24:56 -0600

    initial code done

--- project3/bounded_buffer.c
+++ project3/bounded_buffer.c
@@ -7,43 +7,114 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <semaphore.h>
+#include <unistd.h>
+
+#define SEMKEY ((key_t)400L) // semaphore key
+// #define NSEMS 1              // number of semaphores being created
 
 pthread_t tid1[1];      /* process id for thread 1 */
 pthread_t tid2[1];      /* process id for thread 2 */
 pthread_attr_t attr[1]; /* attribute pointer array */
 
-sem_t sem1;
+// declaring the three semaphores
+sem_t empty;
+sem_t full;
+sem_t mutex;
 
-// void sem_wait(&sem1);
-// void sem_post(&sem1);
-// void sem_init(&sem1, ..., ...);
-// void sem_destroy(&sem1);
+char buffer[15];
+int in = 0;  // pointers for in location in buffer
+int out = 0; // pointers for out location in buffer
+
+void *producer(void *param)
+{
+
+    char newChar; // char for character in file
+    FILE *fp;     // file pointer for file
+
+    // opens file
+    fp = fopen("mytest.dat", "r");
+
+    // loops through the file for each character
+    while (fscanf(fp, "%c", &newChar) != EOF)
+    {
+        sem_wait(&empty); // wait for an empty slot
+        sem_wait(&mutex); // ensure mutual exclusion
+
+        // printf("%c", newChar); // replace this with a line that adds the char to the buffer
+        buffer[in] = newChar;
+        in = (in + 1) % 15;
+
+        sem_post(&mutex); // release mutual exclusion
+        sem_post(&full);  // signal that new data is in the buffer
+    }
+
+    sem_wait(&empty); // wait for an empty slot
+    sem_wait(&mutex); // ensure mutual exclusion
+
+    buffer[in] = '*';
+    in = (in + 1) % 15;
+
+    sem_post(&mutex); // release mutual exclusion
+    sem_post(&full);  // signal that new data is in the buffer
+
+    fclose(fp);
+    pthread_exit(NULL); // idk about this one
+}
+
+void *consumer(void *param)
+{
+
+    char newChar;
+
+    while (1)
+    {
+        sem_wait(&full);
+        sem_wait(&mutex);
+
+        newChar = buffer[out]; // grabs from the buffer
+        out = (out + 1) % 15;
+
+        sem_post(&mutex);
+        sem_post(&empty);
+
+        if (newChar == '*') // if reached end of file, then just die
+        {
+            break;
+        }
+
+        printf("%c", newChar); // used to debug
+        fflush(stdout);
+        sleep(0.5); // so consumer will run slower than producer
+    }
+}
 
 int main()
 {
-    printf("Hello world! \n");
 
-    fflush(stdout);
+    // initialize semaphores
+    sem_init(&empty, 0, 15);
+    sem_init(&full, 0, 0);
+    sem_init(&mutex, 0, 1);
+
     /* Required to schedule thread independently.*/
     pthread_attr_init(&attr[0]);
     pthread_attr_setscope(&attr[0], PTHREAD_SCOPE_SYSTEM);
-    /* end to schedule thread independently */
 
-    /* Create the threads */
-    pthread_create(&tid1[0], &attr[0], thread1, NULL);
-    pthread_create(&tid2[0], &attr[0], thread2, NULL);
+    // producer and consumer threads
+    pthread_create(&tid1[0], &attr[0], producer, NULL);
+    pthread_create(&tid2[0], &attr[0], consumer, NULL);
 
     /* Wait for the threads to finish */
     pthread_join(tid1[0], NULL);
     pthread_join(tid2[0], NULL);
-    // ....................................................Terminate threads
+
+    // terminates threads
     pthread_exit(NULL);
 
-    /* Below is a piece of “C” code that gives you some idea of how to open file and read from file
-    char newChar;
-    FILE *fp;
-    fp = fopen("mytest.dat", "r");
-    while (fscanf(fp, "%c", &newChar) != EOF) */
+    // destroys semaphores
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    sem_destroy(&mutex);
 
     return 0;
 }
