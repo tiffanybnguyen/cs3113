78be643f734752d49eaecc3d407f238d7d6c703d
Tiffany Nguyen <tiffanynguyenb4@gmail.com>
2024-11-26 17:30:49 -0600

    hopefully last commit

--- project3/bounded_buffer.c
+++ project3/bounded_buffer.c
@@ -1,3 +1,8 @@
+/**
+ * Name: Tiffany Nguyen
+ * VM: cs093.cs.ourcloud.ou.edu
+ */
+
 #define _REENTRANT
 #include <pthread.h>
 #include <stdio.h>
@@ -10,7 +15,6 @@
 #include <unistd.h>
 
 #define SEMKEY ((key_t)400L) // semaphore key
-// #define NSEMS 1              // number of semaphores being created
 
 pthread_t tid1[1];      /* process id for thread 1 */
 pthread_t tid2[1];      /* process id for thread 2 */
@@ -38,23 +42,22 @@ void *producer(void *param)
     while (fscanf(fp, "%c", &newChar) != EOF)
     {
         sem_wait(&empty); // wait for an empty slot
-        sem_wait(&mutex); // ensure mutual exclusion
+        sem_wait(&mutex); // wait for mutex
 
-        // printf("%c", newChar); // replace this with a line that adds the char to the buffer
         buffer[in] = newChar;
         in = (in + 1) % 15;
 
-        sem_post(&mutex); // release mutual exclusion
+        sem_post(&mutex); // release mutex
         sem_post(&full);  // signal that new data is in the buffer
     }
 
     sem_wait(&empty); // wait for an empty slot
-    sem_wait(&mutex); // ensure mutual exclusion
+    sem_wait(&mutex); // wait for mutex
 
     buffer[in] = '*';
     in = (in + 1) % 15;
 
-    sem_post(&mutex); // release mutual exclusion
+    sem_post(&mutex); // release mutex
     sem_post(&full);  // signal that new data is in the buffer
 
     fclose(fp);
@@ -68,7 +71,7 @@ void *consumer(void *param)
 
     while (1)
     {
-        sem_wait(&full);
+        sem_wait(&full); // wait until 
         sem_wait(&mutex);
 
         newChar = buffer[out]; // grabs from the buffer
@@ -77,21 +80,21 @@ void *consumer(void *param)
         sem_post(&mutex);
         sem_post(&empty);
 
-        if (newChar == '*') // if reached end of file, then just die
+        if (newChar == '*') // asterisk to symbolize end of file; breaks loop
         {
             break;
         }
 
-        printf("%c", newChar); // used to debug
+        printf("%c", newChar);
         fflush(stdout);
-        sleep(1); // so consumer will run slower than producer
+        sleep(1); // 1 sec sleep so consumer will run slower than producer
     }
 }
 
 int main()
 {
 
-    // initialize semaphores
+    /* initialize semaphores */
     sem_init(&empty, 0, 15);
     sem_init(&full, 0, 0);
     sem_init(&mutex, 0, 1);
@@ -100,7 +103,7 @@ int main()
     pthread_attr_init(&attr[0]);
     pthread_attr_setscope(&attr[0], PTHREAD_SCOPE_SYSTEM);
 
-    // producer and consumer threads
+    /* producer and consumer threads */
     pthread_create(&tid1[0], &attr[0], producer, NULL);
     pthread_create(&tid2[0], &attr[0], consumer, NULL);
 
@@ -108,10 +111,10 @@ int main()
     pthread_join(tid1[0], NULL);
     pthread_join(tid2[0], NULL);
 
-    // terminates threads
+    /* terminates threads */
     pthread_exit(NULL);
 
-    // destroys semaphores
+    /* destroys semaphores */
     sem_destroy(&empty);
     sem_destroy(&full);
     sem_destroy(&mutex);
